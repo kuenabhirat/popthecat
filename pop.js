f53050b8fb7aff515eeba0c25db17378927879b6
kuenabhirat <50983951+kuenabhirat@users.noreply.github.com>
2021-08-17 13:29:07 +0700

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,13 +1,15 @@
 
 // with random char and delay time
 
-var event = new KeyboardEvent('keydown', {
-	key: ''+(Math.random() + 1).toString(36).substring(7).charAt(0)
-});
 setInterval(function(){
+    var event = new KeyboardEvent('keydown', {
+        key: ''+(Math.random() + 1).toString(36).substring(7).charAt(0)
+    });
+    console.log(event.key);
     var i = Math.floor(Math.random() * 100);
     if(i%2==0){
 	    setTimeout(() => { document.dispatchEvent(event); }, i*50);
     }
 }, Math.floor(Math.random() * 100));
+
 
