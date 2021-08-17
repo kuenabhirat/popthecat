a859eba960cf90c4275e4e0e79a8fc01872cd02e
kuenabhirat <50983951+kuenabhirat@users.noreply.github.com>
2021-08-17 13:14:46 +0700

    Update pop.js

--- pop.js
+++ pop.js
@@ -4,5 +4,5 @@ var event = new KeyboardEvent('keydown', {
 	key: ''+(Math.random() + 1).toString(36).substring(7).charAt(0)
 });
 setInterval(function(){
-	setTimeout(() => { document.dispatchEvent(event); }, Math.random()*50);
-}, Math.random()*50);
+	setTimeout(() => { document.dispatchEvent(event); }, Math.random()*100);
+}, Math.random()*100);
